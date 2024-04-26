6a0fef7117c190f17a161389d41c99cf2aaf5425
marsStudio21 <95658147+marsStudio21@users.noreply.github.com>
2024-04-26 21:38:20 +0100

    slide 2 update...

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const searchIcon = document.querySelector(".search-icon");
     const popupSearch = document.querySelector(".popupsearch");
     const closeSearch = document.querySelector(".close-search");
+    const addIcon = document.querySelector(".add-icon");
+    const pagerSlide = document.querySelector(".pager");
+    const closePager = document.querySelector(".close-icon2");
+
+    const btnNext = document.querySelector(".next");
+    const slideItem = document.querySelector(".item1");
+
+
 
     searchIcon.addEventListener("click", function() {
         popupSearch.classList.toggle("active");
@@ -11,7 +19,22 @@ document.addEventListener("DOMContentLoaded", function() {
     closeSearch.addEventListener("click", function() {
         popupSearch.classList.remove("active");
     });
+
+    addIcon.addEventListener("click", function() {
+        pagerSlide.classList.toggle("active"); 
+      
+    });
+
+    closePager.addEventListener("click", function() {
+        pagerSlide.classList.remove("active");
+    });
+
+    btnNext.addEventListener("click", function() {
+        slideItem.classList.toggle("unabled");
+        console.log("Je passe au suivant");
+    });
 });
 
 
-console.log("Ma page js est bien liée");
+
+
